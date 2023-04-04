6f265f5ee1e983e6033696a4522ad122bbf00b0c
juantoranzos <toranzos628@gmail.com>
2023-04-03 21:12:25 -0300

    ultimos retoques al codigo js

--- js/numerosMagicos.js
+++ js/numerosMagicos.js
@@ -1,3 +1,12 @@
+/*1- Crea una web con bootstrap y js, que contenga un botón comenzar el juego, 
+en ese momento se crea un número aleatorio que el usuario deberá adivinar, 
+la interfaz del usuario debe tener además un input para ingresar un número y 
+un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario 
+adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero 
+que ingreso es mayor o menor al número mágico.
+Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
+ */
+
 //seleccionar elementos del HTML
 let title = document.querySelector('h1');
 
@@ -42,7 +51,7 @@ btnSubmit.addEventListener('click', () =>{
     numeroRandom = generarNumeroRandom();
 
     numeroInput.value = "";
-    
+
     numeroInput.focus();
     
 })
